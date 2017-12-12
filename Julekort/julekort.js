90bb63954e4ea393189431ff0366c52223ec3713
TheRealOdd <oddeinarlervik@icloud.com>
2017-12-12 09:31:37 +0100

    yey

--- Julekort/julekort.js
+++ Julekort/julekort.js
@@ -1,15 +1,18 @@
-function setup(){
+function setup() {
     let divSky = document.getElementById("sky");
     let divNisse = document.getElementById("nisse");
-    let star = divSky = divSky.querySelectorAll(".star");
+    let stars = divSky.querySelectorAll(".star");
     stars.forEach(stjerne => {
-        stjerne.style.left = -120 + Math.floor(Math.random()*40) + "px";
-        stjerne.style.top = -80 + Math.floor(Math.random()*80) + "px";
-    }); 
-divNisse.addEventListener("click", dropGifts);
-function dropGits(e) {
-    let oakke = document.createElement("div");
-    pakke.className = "pakke";
+        stjerne.style.left = -120 + Math.floor(Math.random() * 240) + "px";
+        stjerne.style.top = -80 + Math.floor(Math.random() * 180) + "px";
+    });
+    divNisse.addEventListener("click", dropGifts);
+    function dropGifts(e) {
+        let pakke = document.createElement("div");
+        pakke.className = "pakke";
+        pakke.style.left = e.screenX + "px";
+        pakke.style.top = (e.screenY - 50) + "px";
+        divSky.appendChild(pakke);
 
-}
     }
+}
